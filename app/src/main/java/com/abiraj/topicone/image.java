2e2f1b940885b6da9d74f7207e77f50d850051a0
abirajtimalsina <timalsinaabiraj92@gmail.com>
2019-11-27 14:03:45 +0545

    Alert Dailogue Box

--- app/src/main/java/com/abiraj/topicone/image.java
+++ app/src/main/java/com/abiraj/topicone/image.java
@@ -1,17 +1,21 @@
 package com.abiraj.topicone;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
-public class image extends AppCompatActivity implements View.OnClickListener
-{
+
+public class image extends AppCompatActivity implements View.OnClickListener {
 
     private RadioButton rdoOpera, rdoCollesum, rdoTaj;
     private ImageView imgv;
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,8 @@ public class image extends AppCompatActivity implements View.OnClickListener
         rdoCollesum.setOnClickListener(this);
         rdoOpera.setOnClickListener(this);
 
+        builder = new AlertDialog.Builder(this);
+
     }
 
     @Override
@@ -34,18 +40,70 @@ public class image extends AppCompatActivity implements View.OnClickListener
         switch (v.getId()) {
 
             case R.id.rdoCo:
-                imgv.setImageResource(R.drawable.collesum);
+                builder.setMessage(" want to load this image ?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                imgv.setImageResource(R.drawable.collesum);
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(image.this, "you clicked no", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                AlertDialog alert = builder.create();
+                alert.setTitle("Do you");
+                alert.show();
+
                 break;
 
             case R.id.rdoOp:
-                imgv.setImageResource(R.drawable.opera);
+                builder.setMessage(" want to load this image ?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                imgv.setImageResource(R.drawable.opera);
+                            }
+                        })
+                        .setNegativeButton("no", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(image.this, "You clicked No", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+
+                AlertDialog alert1 = builder.create();
+                alert1.setTitle("Do you");
+                alert1.show();
+
                 break;
 
             case R.id.rdoTj:
-                imgv.setImageResource(R.drawable.taj);
-                break;
+                builder.setMessage(" want to load this image ?")
+                        .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                imgv.setImageResource(R.drawable.taj);
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(image.this, "You clicked no", Toast.LENGTH_SHORT).show();
+                            }
+                        });
 
+                AlertDialog alert2 = builder.create();
+                alert2.setTitle("Do you");
+                alert2.show();
+
+                break;
         }
+
 
     }
 }
